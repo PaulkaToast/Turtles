34dd66708751fef683d2ee8f8ae5b79d7d3c9c48
Paula Toth <paulatoth18@aim.com>
2017-08-04 14:51:54 -0400

    Adding project to github

---
+++ RandomTurtleTester.java
@@ -0,0 +1,54 @@
+package turtleClasses;
+
+
+public class RandomTurtleTester
+{
+  
+  public static void main (String[] args) 
+  { 
+    int forward =2;
+    World w = new World(1200,1200);
+    Turtle gabe = new Turtle(600,600,w);
+    Turtle paula = new Turtle(600,600,w);
+    Turtle bernard = new Turtle(600,600,w);
+    Turtle zoltab = new Turtle(600,600,w);
+    paula.turn(90);
+    bernard.turn(180);
+    zoltab.turn(270);
+    
+    String directions = "R";
+    
+    
+    for (int j = 0; j < 16; j++) {
+      String next = directions;
+      String inverse = "";
+      for (int i = next.length() - 1; i >= 0; i--) {
+        inverse += next.charAt(i) == 'R' ? 'L' : 'R';
+      }
+      directions = next + "R" + inverse;
+      System.out.println(directions);
+    }
+    int count = 0;
+    for (count = 0; count < directions.length(); count++) {
+      if (directions.charAt(count) == 'R') {
+        gabe.turnRight();
+        paula.turnRight(); 
+        bernard.turnRight();
+        zoltab.turnRight();
+      }
+      else {
+        gabe.turnLeft();
+        paula.turnLeft();
+        bernard.turnLeft();
+        zoltab.turnLeft();
+      }
+      paula.forward(forward);
+      gabe.forward(forward);
+      zoltab.forward(forward);
+      bernard.forward(forward);
+    }
+    
+  }
+  
+} 
+
