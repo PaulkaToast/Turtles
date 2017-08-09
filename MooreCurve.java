a99b52cecbbb1d3f328e1f9aa6ba8e8ddeb3b99c
Paula Toth <paulatoth18@aim.com>
2017-08-09 18:59:28 -0400

    Add colors to Left and Right curve branches.

--- MooreCurve.java
+++ MooreCurve.java
@@ -1,15 +1,33 @@
 package turtleClasses;
 
+import java.awt.Color;
+
 public class MooreCurve {
 	
 	public static Turtle t;
-	public static final int MAX = 6;
-	public static final int F = 10;
+	public static final int MAX = 7;
+	public static final int F = 5;
+	
+	public static int color = 0;
+	
+	/*public static final Color[] colors = {
+		Color.RED, 
+		Color.ORANGE,
+		Color.YELLOW,
+		Color.GREEN,
+		Color.CYAN,
+		Color.BLUE,
+		Color.MAGENTA,
+		Color.PINK,
+		Color.GRAY,
+		Color.BLACK
+		};*/
 	
 	public static void LMoores(int depth) {
 		if (depth >= MAX) {
 			return;
 		}
+		t.setColor(Color.BLUE);
 		t.turnLeft(); 		//-
 		RMoores(depth + 1); //R
 		t.forward(F); 		//F
@@ -21,11 +39,13 @@ public class MooreCurve {
 		t.forward(F);		//F
 		RMoores(depth + 1);	//R
 		t.turnLeft(); 		//-
+		//t.setColor(colors[color++ % colors.length]);
 	}
 	public static void RMoores(int depth) {
 		if (depth >= MAX) {
 			return;
 		}
+		t.setColor(Color.RED);
 		t.turnRight(); 		//+
 		LMoores(depth + 1); //L
 		t.forward(F);  		//F
@@ -37,12 +57,12 @@ public class MooreCurve {
 		t.forward(F);		//F
 		LMoores(depth + 1); //L
 		t.turnRight(); 		//+
+		//t.setColor(colors[depth % colors.length]);
 	}
 	public static void main (String[] args) { 
 	    World w = new World(1000,1000);
 	    
 	    t = new Turtle(1000,1000,w);
-	    
 	    LMoores(0);
     }
 }  
