fadb1000dc87a2e3c9176d7441624d9af00d99ad
Paula Toth <paulatoth18@aim.com>
2017-08-09 20:46:09 -0400

    Add Fibonacci word fractal.

---
+++ FibonacciWord.java
@@ -0,0 +1,51 @@
+package turtleClasses;
+
+import java.awt.Color;
+
+public class FibonacciWord {
+	public static Turtle t;
+	public static final int MAX = 21;
+	public static final int F = 2;
+	public static int n = 0;
+
+	public static void expand(String num, int depth) {
+		if (depth > MAX) {
+			return;
+		}
+		
+		StringBuilder builder = new StringBuilder();
+		for (char c : num.toCharArray()) {
+			if (c == '1') {
+				builder.append('0');
+			} else if (c == '0') {
+				builder.append("01");
+			}
+		}
+		String newNum = builder.toString();
+		//System.out.println(newNum);
+		if (depth == MAX) {
+		for (int i = 0; i < newNum.length(); i++) {
+			t.forward(F);
+			t.setColor(Color.RED);
+			char c = newNum.charAt(i);
+			if (c == '0') {
+				t.setColor(Color.BLUE);
+				if ((i + 1) % 2 == 0) {
+					t.turnLeft();
+				} else {
+					t.turnRight();
+				}
+			}
+		}
+		}
+		
+		expand(newNum, depth + 1);
+	}
+	
+	public static void main (String[] args) { 
+	    World w = new World(1200,1000);
+	    
+	    t = new Turtle(0,1000,w);
+	    expand("1", 0);
+    }
+}
