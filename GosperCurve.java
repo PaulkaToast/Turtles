54bbb011c785eec338fc51100bd407b0cdf5ff0e
Paula Toth <paulatoth18@aim.com>
2017-08-10 00:00:48 -0400

    Add Gosper Curve.

---
+++ GosperCurve.java
@@ -0,0 +1,50 @@
+package turtleClasses;
+
+import java.awt.Color;
+
+public class GosperCurve {
+	public static Turtle t;
+	public static final int MAX = 4;
+	public static final int F = 5;
+	public static int n = 0;
+
+	public static void expand(String num, int depth) {
+		if (depth > MAX) {
+			return;
+		}
+		
+		StringBuilder builder = new StringBuilder();
+		for (char c : num.toCharArray()) {
+			if (c == 'A') {
+				builder.append("A-B--B+A++AA+B-");
+			} else if (c == 'B') {
+				builder.append("+A-BB--B-A++A+B");
+			} else {
+				builder.append(c);
+			}
+		}
+		String newNum = builder.toString();
+		//System.out.println(newNum);
+		if (depth == MAX) {
+			for (int i = 0; i < newNum.length(); i++) {
+				char c = newNum.charAt(i);
+				if (c == 'A' || c == 'B') {
+					t.forward(F);
+				}else if (c == '+') {
+					t.turn(60);
+				}else if ( c == '-') {
+					t.turn(-60);
+				}
+			}
+		}
+		
+		expand(newNum, depth + 1);
+	}
+	
+	public static void main (String[] args) { 
+	    World w = new World(1200,1000);
+	    
+	    t = new Turtle(1000,300,w);
+	    expand("A", 0);
+    }
+}
